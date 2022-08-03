f8137c278394ec646197018779e731e59bf9f56b
João Goncalves <joaov.gonca@gmail.com>
2022-08-03 00:27:04 -0300

    Add Pokemon component v2

---
+++ src/components/pokemon/v2/Pokemon.js
@@ -0,0 +1,12 @@
+import styles from "./Pokemon.module.css";
+
+function Pokemon(props){
+    return (
+    <div className={styles.container}>
+            <div className={styles.number}>#001</div>
+            <div className={styles.name}>Charizard</div>
+            <div className={styles.poke-container}></div>
+    </div>)
+}
+
+export default Pokemon;
